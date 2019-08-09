24407e1ae0a89243bbc20a394d35175fa7fd6c95
Thiroux-Collard Jonathan <thirouxjonathan@yahoo.fr>
2019-08-09 10:43:00 +0200

    rename class and remove useless import

--- src/fr/jonathanTC/src/Main.java
+++ src/fr/jonathanTC/src/Main.java
@@ -3,8 +3,7 @@ package fr.jonathanTC.src;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Fenetre f = new Fenetre("SQL Query Tester");
+		Window f = new Window("SQL Query Tester");
 		f.setVisible(true);
 	}
 
