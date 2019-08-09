4271eb1909310d87ebfac617ab99327f98f3034c
Thiroux-Collard Jonathan <thirouxjonathan@yahoo.fr>
2019-08-09 10:31:31 +0200

    initial commit

---
+++ src/fr/jonathanTC/src/Main.java
@@ -0,0 +1,11 @@
+package fr.jonathanTC.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Fenetre f = new Fenetre("SQL Query Tester");
+		f.setVisible(true);
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,5 @@
+module RequestTester {
+	requires java.desktop;
+	requires java.sql;
+	exports fr.jonathanTC.src;
+}
